0d30276b92996bc1aa99fad61f2f268f87ff5a91
Sami <sami@example.com>
2012-10-09 17:04:11 +0300

    Laboratorul 1. Commit initial

---
+++ lab1/doi.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use warnings;
+@elevi = ("Petru", "Carmen", "Marius");
+@colectie = ("Petru", 9.7, "Carmen", 8, "Marius", "Angela", 'c');
+print $elevi[0];            #primul element din lista @elevi.
+print $elevi[-1];           #ultimul element din lista elevi.
+print scalar(@elevi);     #numarul de elemente 
+print $#elevi;            #indexul maxim dintr-o lista (scalar(@elevi)-1)
+
---
+++ lab1/unu.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+
+
+
+{
+@elevi = ("Petru", "Carmen", "Marius");
+@colectie = ("Petru", 9.7, "Carmen", 8, "Marius", "Angela", 'c');
+
+print $elevi[0];            #primul element din lista @elevi.
+print $elevi[-1];           #ultimul element din lista elevi.
+
+print scalar(@elevi);     #numarul de elemente 
+print $#elevi;            #indexul maxim dintr-o lista (scalar(@elevi)-1)
+}
